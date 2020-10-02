467003073a27fe8717aa4ad577e8375f7c190255
Nick Sheck <sheck@users.noreply.github.com>
2020-10-02 16:35:41 -0700

    Handle Rubocop failures better (#9)
    * Make sure we don't leave the action running
    
    * Lets see if it breaks the right way
    
    * Revert "Lets see if it breaks the right way"
    
    This reverts commit c239d77c4cdc663c439bb6a5629aa7fb1d4d0275.
    
    * Remove debug for success case

--- action/action.rb
+++ action/action.rb
@@ -33,14 +33,6 @@ if !check_run_create.ok?
   raise "Couldn't create check run #{check_run_create.inspect}"
 end
 
-compare_sha = event.pull_request.base.sha
-
-rubocop_json = Bundler.with_original_env do
-  `git diff --name-only #{compare_sha} --diff-filter AM --relative | xargs rubocop --force-exclusion --format json`
-end
-
-rubocop_output = JSON.parse(rubocop_json, object_class: OpenStruct)
-
 RUBOCOP_TO_GITHUB_SEVERITY = {
   "refactor" => "failure",
   "convention" => "failure",
@@ -48,8 +40,6 @@ RUBOCOP_TO_GITHUB_SEVERITY = {
   "error" => "failure",
   "fatal" => "failure"
 }.freeze
-
-annotations = []
 
 def git_root
   @git_root ||= Pathname.new(GitUtils.root)
@@ -67,25 +57,44 @@ def file_fullpath(relative_path)
   end
 end
 
-rubocop_output.files.each do |file|
-  path = file_fullpath(file.path)
+def generate_annotations(compare_sha:)
+  annotations = []
 
-  change_ranges = GitUtils.generate_change_ranges(path, compare_sha: compare_sha)
-
-  file.offenses.each do |offense|
-    next unless change_ranges.any? { |range| range.include?(offense.location.start_line) }
-
-    annotations.push(
-      path: path,
-      start_line: offense.location.start_line,
-      end_line: offense.location.last_line,
-      annotation_level: RUBOCOP_TO_GITHUB_SEVERITY[offense.severity],
-      message: offense.message
-    )
+  rubocop_json = Bundler.with_original_env do
+    `git diff --name-only #{compare_sha} --diff-filter AM --relative | xargs rubocop --force-exclusion --format json`
   end
+
+  rubocop_output = JSON.parse(rubocop_json, object_class: OpenStruct)
+
+  rubocop_output.files.each do |file|
+    path = file_fullpath(file.path)
+
+    change_ranges = GitUtils.generate_change_ranges(path, compare_sha: compare_sha)
+
+    file.offenses.each do |offense|
+      next unless change_ranges.any? { |range| range.include?(offense.location.start_line) }
+
+      annotations.push(
+        path: path,
+        start_line: offense.location.start_line,
+        end_line: offense.location.last_line,
+        annotation_level: RUBOCOP_TO_GITHUB_SEVERITY[offense.severity],
+        message: offense.message
+      )
+    end
+  end
+
+  annotations
 end
 
-resp = check_run.update(annotations: annotations)
-
-p resp
-p resp.json
+begin
+  annotations = generate_annotations(compare_sha: event.pull_request.base.sha)
+rescue Exception => e
+  puts e.message
+  puts e.backtrace.inspect
+  resp = check_run.error(message: e.message)
+  p resp
+  p resp.json
+else
+  check_run.update(annotations: annotations)
+end
