a0c67c03a3582174e3f0dcc08784c9bdf80a5fbe
Daniel Ma <dma@planningcenter.com>
2021-05-14 15:26:25 -0700

    test: make string quotes an error level offense

--- test/code.rb
+++ test/code.rb
@@ -1,5 +1,5 @@
-puts 'at least one of these lines'
-puts "is guaranteed to upset rubocop"
+puts "at least one of these lines"
+puts 'is guaranteed to upset rubocop'
 
 and_if_not = { this: :really, :ought => :to_do_it }
 
